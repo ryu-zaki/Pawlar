9c9a8e8edd6f3efcf2b4e75eb8666f6645c5895d
Sandra Agustin <Agustinsandy03@gmail.com>
2025-10-18 15:50:40 +0800

    resolve conflict

--- front-end/capacitor.config.ts
+++ front-end/capacitor.config.ts
@@ -4,12 +4,8 @@ const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'capacitor_prac',
   webDir: 'dist',
-  server: {
-<<<<<<< HEAD
+  server: { 
     url: "http://192.168.123.214:5173/",
-=======
-    url: "http://192.168.123.150:5173",
->>>>>>> 0a491ce22c835a948e117f7484cd750eb05353b6
     cleartext: true
   }
 };
