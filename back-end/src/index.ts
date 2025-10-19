d94d10068ae9e0a0b8da6804653f0c0d5c1d1c8d
ryu-zaki <jhonwellryuzaki0987@gmail.com>
2025-10-19 17:50:02 +0800

    access token check

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -3,7 +3,7 @@ import routes from './routes';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
-import { refreshPage } from './middlewares/refresh.middleware';
+import { checkIfAccessToken } from "./middlewares/refresh.middleware";
 
 const app = express();
 dotenv.config();
@@ -13,13 +13,6 @@ app.use(cookieParser());
 app.use(express.json());
 app.use('/', routes);
 
-app.use(refreshPage);
-app.get('/profile', (req, res) => {
-   const authHeaders = req.headers["authorization"];
-   const auth = authHeaders && authHeaders.split(' ')[1];
-   
-   console.log(auth);
-   res.sendStatus(200);
-})
+app.get('/check', checkIfAccessToken)
  
 app.listen(3000, () => console.log("Server is runninng on PORT 3000"));
