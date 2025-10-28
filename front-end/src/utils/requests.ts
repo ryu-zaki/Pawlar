33de914dbb928177a72e3ae1d3b57720e044c537
Kenzo Vidal <kz.codesofficial@gmail.com>
2025-10-29 00:01:19 +0800

    Added functions - requestPasswordReset, resetPassword

--- front-end/src/utils/requests.ts
+++ front-end/src/utils/requests.ts
@@ -13,4 +13,23 @@ const response = await api.post('/profile');
   console.log(response.data);
 }
 
-export { loginRequest, accessProfile }
+interface ApiResponse {
+  message: string;
+}
+
+const requestPasswordReset = async (email: string) => {
+    const response = await api.post<ApiResponse>('/auth/forgot-password', { email });
+    return response.data; 
+};
+
+const resetPassword = async (email: string, otp: string, newPassword: string) => {
+    const response = await api.post<ApiResponse>('/auth/reset-password', {
+        email,
+        otp,
+        newPassword,
+    });
+    return response.data; 
+};
+
+
+export { loginRequest, accessProfile, requestPasswordReset, resetPassword }
