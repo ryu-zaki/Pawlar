7ee5389f9d5e2f368a14382c3c569bd74818ca71
ryu-zaki <jhonwellespanola4@gmail.com>
2025-11-05 23:39:51 +0800

    increase from 500 KB to 1 MB

--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -5,4 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  
+  build: {
+    chunkSizeWarningLimit: 1000, // increase from 500 KB to 1 MB
+  },
 })
