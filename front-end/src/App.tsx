7256cc7ff3acd7321c7c92ba47df3db7dae64524
Kenzo Vidal <kz.codesofficial@gmail.com>
2025-11-05 20:36:18 +0800

    Adjusted redirect path

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -24,26 +24,27 @@ function App() {
 
     <>
       <Toaster richColors position="bottom-center" />
-      
+
       <Routes>
         <Route path='/' element={<StartupPage />} />
 
-         <Route path="/auth" element={isLogin ? (isEmailVerified ? <Navigate to="/sample" /> : <Navigate to="/verify-signup"  /> ) : <AuthLayout />}>
+        <Route path="/auth" element={isLogin ? (isEmailVerified ? <Navigate to="/sample" /> : <Navigate to="/verify-signup" />) : <AuthLayout />}>
 
           <Route path='login' element={<LoginPage />} />
           <Route path='signup' element={<SignUpPage />} />
           <Route path="termsandconditions" element={< TermsAndConditions />} />
+          
           <Route path="otp" element={<ForgotPasswordParent />}>
             <Route index element={<EmailSendOTP />} />
             <Route path="verify" element={<EmailOTP />} />
             <Route path="renew" element={<RenewPassword />} />
           </Route>
 
+          <Route path="verify-signup" element={!isEmailVerified ? <SignupEmailOTP /> : <Navigate to="/auth/login" />} />
         </Route>
-        
-        <Route path="/verify-signup" element={!isEmailVerified ? <SignupEmailOTP /> : <Navigate  to="/auth/login" />} />
 
-        <Route path="/sample" element={isLogin ? (isEmailVerified ? <SampleLandingPage /> : <Navigate to="/verify-signup"  /> ) : <Navigate to="/auth/login" />} />
+
+        <Route path="/sample" element={isLogin ? (isEmailVerified ? <SampleLandingPage /> : <Navigate to="/verify-signup" />) : <Navigate to="/auth/login" />} />
       </Routes>
 
     </>
