3cf60b26999789dc7bd69949d6798943badb3918
ryu-zaki <jhonwellespanola4@gmail.com>
2025-11-07 08:28:41 +0800

    OTP error handler

--- back-end/src/utils/email.helper.ts
+++ back-end/src/utils/email.helper.ts
@@ -2,6 +2,8 @@ import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
   service: 'gmail', 
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER, 
     pass: process.env.EMAIL_APP_PASSWORD, 
