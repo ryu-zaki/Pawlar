e3a0a8e39b830fb7b273970b3d3517678fc65c43
ryu-zaki <jhonwellespanola4@gmail.com>
2025-11-07 09:19:35 +0800

    gmail service

--- back-end/src/utils/email.helper.ts
+++ back-end/src/utils/email.helper.ts
@@ -4,7 +4,7 @@ const transporter = nodemailer.createTransport({
    tls: {
       rejectUnauthorized: false
     },
-    service: 'hotmail',
+    service: 'gmail',
   auth: {
     user: process.env.EMAIL_USER, 
     pass: process.env.EMAIL_APP_PASSWORD, 
