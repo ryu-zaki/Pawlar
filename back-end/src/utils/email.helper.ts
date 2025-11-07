9c2aec53e9f5728f12a2ffcfc07fa5cf1bd1ad70
ryu-zaki <jhonwellespanola4@gmail.com>
2025-11-07 09:07:16 +0800

    change service

--- back-end/src/utils/email.helper.ts
+++ back-end/src/utils/email.helper.ts
@@ -1,7 +1,10 @@
 import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail', 
+   tls: {
+      rejectUnauthorized: false
+    },
+    service: 'hotmail',
   auth: {
     user: process.env.EMAIL_USER, 
     pass: process.env.EMAIL_APP_PASSWORD, 
